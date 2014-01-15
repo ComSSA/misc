5f7ffa75e05bdcaa173435d8090935e99b3f291d
Delan Azabani <delan@azabani.com>
2014-01-16 01:07:53 +0800

    configure HE IPv6 tunnel in setup script

--- servers/ling/setup.sh
+++ servers/ling/setup.sh
@@ -19,6 +19,19 @@ package () {
 	apt-get -qq install $1 > /dev/null
 }
 
+log Configuring Hurricane Electric IPv6 tunnel
+echo >> /etc/network/interfaces
+echo 'auto he-ipv6' >> /etc/network/interfaces
+echo 'iface he-ipv6 inet6 v4tunnel' >> /etc/network/interfaces
+echo 'address 2001:db8:0f10:99d::2' >> /etc/network/interfaces
+echo 'netmask 64' >> /etc/network/interfaces
+echo 'endpoint 209.51.181.2' >> /etc/network/interfaces
+echo 'ttl 255' >> /etc/network/interfaces
+echo 'gateway 2001:db8:0f10:99d::1' >> /etc/network/interfaces
+echo >> /etc/network/interfaces
+ifup he-ipv6
+ping6 -c1 google.com > /dev/null
+
 log Adding user group: committee
 groupadd committee
 
