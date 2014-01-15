0f90b2ec414cded76d0b68a274d03c92b5bc2bd6
Delan Azabani <delan@azabani.com>
2014-01-16 01:10:07 +0800

    actually fill in the relevant addresses, of course

--- servers/ling/setup.sh
+++ servers/ling/setup.sh
@@ -23,11 +23,11 @@ log Configuring Hurricane Electric IPv6 tunnel
 echo >> /etc/network/interfaces
 echo 'auto he-ipv6' >> /etc/network/interfaces
 echo 'iface he-ipv6 inet6 v4tunnel' >> /etc/network/interfaces
-echo 'address 2001:db8:0f10:99d::2' >> /etc/network/interfaces
+echo 'address 2001:470:1f14:116e::2' >> /etc/network/interfaces
 echo 'netmask 64' >> /etc/network/interfaces
-echo 'endpoint 209.51.181.2' >> /etc/network/interfaces
+echo 'endpoint 216.66.84.46' >> /etc/network/interfaces
 echo 'ttl 255' >> /etc/network/interfaces
-echo 'gateway 2001:db8:0f10:99d::1' >> /etc/network/interfaces
+echo 'gateway 2001:470:1f14:116e::1' >> /etc/network/interfaces
 echo >> /etc/network/interfaces
 ifup he-ipv6
 ping6 -c1 google.com > /dev/null
